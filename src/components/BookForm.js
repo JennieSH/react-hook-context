ec3639dcfa2805573351778c1b72d20bd34b6f59
Jennie <m65pjj@gmail.com>
2020-04-29 02:14:07 +0800

    Add useReducer

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,13 +2,15 @@ import React, { useContext, useState } from "react";
 import { BookContext } from "../context/BookContext";
 
 const BookForm = () => {
-    const { addBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
     const [ title, setTitle ] = useState("");
     const [ author, setAuthor ] = useState("");
 
     const handleAddBook = (e) => {
         e.preventDefault();
-        addBook(title, author);
+        dispatch({ type:"ADD_BOOK", book: {
+            title,author
+        }})
         setTitle("");
         setAuthor("");
     }
